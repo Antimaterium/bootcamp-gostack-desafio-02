97f6a44ad77a3e34f671ccd240e806cc8c5adeff
Antimaterium <vitorafonso33@gmail.com>
2019-11-15 14:08:08 -0200

    Create user model and init sequelize db connection

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,17 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
+
 const routes = new Router();
 
-routes.get('/', (req, res) =>
-  res.json({
-    message: 'main routes',
-  })
-);
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Teste',
+    email: 'teste@teste.com',
+    password_hash: '12345678',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
