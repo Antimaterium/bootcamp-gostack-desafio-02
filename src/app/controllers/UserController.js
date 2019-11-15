d0ed6155b9928a065423f433a86455defb777ec6
Antimaterium <vitorafonso33@gmail.com>
2019-11-15 14:25:55 -0200

    Create user controller

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,16 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    const userExists = await User.findOne({ where: { email: req.body.email } });
+    if (userExists) {
+      return res.status(400).json({ error: 'User alredy exists' });
+    }
+
+    const user = await User.create(req.body);
+    const { id, name, email, provider } = user;
+    return res.json({ id, name, email, provider });
+  }
+}
+
+export default new UserController();
