856aee9af0e424d25f796f23564f6ce7723fc09d
Antimaterium <vitorafonso33@gmail.com>
2019-11-16 13:00:53 -0200

    auth middleware

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -11,6 +11,10 @@ class UserController {
     const { id, name, email, provider } = user;
     return res.json({ id, name, email, provider });
   }
+
+  async update(req, res) {
+    return res.json({ ok: true });
+  }
 }
 
 export default new UserController();
